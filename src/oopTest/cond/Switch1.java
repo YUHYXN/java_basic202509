1a4ea582ac509fa11118e5a47a42e473034eea37
woou <hxx_ara@naver.com>
2025-10-15 18:25:49 +0900

    switch문 - 새로운 switch문과 기존 switch문의 차이

--- src/oopTest/cond/Switch1.java
+++ src/oopTest/cond/Switch1.java
@@ -3,6 +3,9 @@ package oopTest.cond;
 public class Switch1 {
     public static void main(String[] args) {
 
+        //스위치문은 if문을 좀더 편리하게 사용할 수 있지만,
+        //if문은 비교 연산자를 사용할 수 있지만, 스위치문은 값이 같은지 단순하게 비교만 가능하다
+
         int grade = 2;
 
         int coupon;
--- src/oopTest/cond/Switch4.java
+++ src/oopTest/cond/Switch4.java
@@ -8,6 +8,8 @@ public class Switch4 {
         int grade = 2;
 
         int coupon = switch (grade){
+            // 결과 값이 바로 변수에 대입이 되어서 깔끔하게 사용 가능.
+            // 기존 스위치문과는 -> 를 사용한다는 것과 break 문이 필요 없다는 것.
             case 1 -> 1000;
             case 2 -> 2000;
             case 3 -> 3000;
